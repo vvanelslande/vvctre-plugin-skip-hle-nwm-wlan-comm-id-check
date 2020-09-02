8a5ed1960a9001ea6ecaf72ba671d66f579f182e
Valentin Vanelslande <vvanelslandedev@gmail.com>
2020-09-02 02:34:47 -0500

    Initial commit

---
+++ plugin.c
@@ -0,0 +1,27 @@
+// Copyright 2020 Valentin Vanelslande
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include <stdbool.h>
+#include "common_types.h"
+
+#ifdef _WIN32
+#define VVCTRE_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define VVCTRE_PLUGIN_EXPORT
+#endif
+
+VVCTRE_PLUGIN_EXPORT int GetRequiredFunctionCount() {
+    return 0;
+}
+
+VVCTRE_PLUGIN_EXPORT const char** GetRequiredFunctionNames() {
+    return NULL;
+}
+
+VVCTRE_PLUGIN_EXPORT void PluginLoaded(void* core, void* plugin_manager,
+                                       void* required_functions[]) {}
+
+VVCTRE_PLUGIN_EXPORT bool OverrideWlanCommIdCheck(u32 in_beacon, u32 requested) {
+    return true;
+}
